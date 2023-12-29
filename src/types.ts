0169b67d4c890661d4f9cb6762012b8566de3313
Graig Janssen <graig.janssen@gmail.com>
2023-12-29 13:35:52 -0500

    define daily entry types

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+export interface DietEntry {
+  meal: "breakfast" | "lunch" | "dinner" | "snack";
+  healthRating: 1 | 2 | 3 | 4 | 5;
+}
+
+type ExerciseStrengthEntry = {
+  exerciseName: string;
+  type: 'strength';
+  sets: number;
+  reps: number;
+}
+
+type ExerciseCardioEntry = {
+  exerciseName: string;
+  type: 'cardio';
+  minutes: number;
+}
+
+export type ExerciseEntry = ExerciseStrengthEntry | ExerciseCardioEntry;
+
+export interface SleepEntry {
+  hours: number;
+  quality: 1 | 2 | 3 | 4 | 5;
+}
+
+export interface DailyEntry {
+  date: string;
+  moodRating: 1 | 2 | 3 | 4 | 5 | 6 | 7;
+  moodDescription?: string;
+  journalEntry?: string;
+  diet?: DietEntry[];
+  exercise?: ExerciseEntry[];
+  sleep?: SleepEntry;
+}
