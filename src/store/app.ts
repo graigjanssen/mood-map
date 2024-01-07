1621ea892398c9733cd5c27e41c50fb766fb624c
Graig Janssen <graig.janssen@gmail.com>
2024-01-07 16:45:53 -0500

    main pinia store

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,8 +1,28 @@
 // Utilities
-import { defineStore } from 'pinia'
-
+import { defineStore } from 'pinia';
+import { DailyEntry } from '@/types';
 export const useAppStore = defineStore('app', {
   state: () => ({
-    //
+    entries: [] as DailyEntry[],
   }),
-})
+  getters: {
+    latestEntry: (state) => {
+      return state.entries.length
+        ? state.entries[state.entries.length - 1]
+        : null;
+    },
+  },
+  actions: {
+    addEntry(entry: DailyEntry) {
+      this.entries.push(entry);
+    },
+    editEntry(updatedEntry: DailyEntry) {
+      const index = this.entries.findIndex(
+        (entry) => entry.date === updatedEntry.date
+      );
+      if (index !== -1) {
+        this.entries[index] = updatedEntry;
+      }
+    },
+  },
+});
